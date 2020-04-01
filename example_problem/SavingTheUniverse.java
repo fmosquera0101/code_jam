360170b99662470e505100b1fe7e332773241126
Fredy Mosquera <fmosquera0101@gmail.com>
2020-04-01 00:00:01 -0500

    Exercise round qulification Saving the world

---
+++ example_problem/SavingTheUniverse.java
@@ -0,0 +1,45 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SavingTheUniverse {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
+        int n  = Integer.parseInt(in.nextLine());
+
+        for (int i = 1; i <= n ; i++) {
+
+            int s = Integer.parseInt(in.nextLine());
+            List<String> searchEngines = new ArrayList<>(s);
+            for (int j = 1; j <= s; j++) {
+                searchEngines.add(in.nextLine());
+            }
+
+            int q = Integer.parseInt(in.nextLine());
+            List<String> queries = new ArrayList<>(q);
+            for (int j = 1; j <= q ; j++) {
+                queries.add(in.nextLine());
+            }
+
+            long min = searchEngines.stream()
+                    .map(
+                            engine -> queries.stream()
+                                    .filter( query -> query.equals(engine))
+                    )
+                    .map(c -> c.count())
+                    .min(Long::compare).get();
+
+            System.out.println("Case #"+i+": "+min);
+        }
+
+
+
+
+    }
+}
---
+++ src/main/java/co/com/fredymosquera/round2008/SavingTheUniverse.java
@@ -0,0 +1,4 @@
+package co.com.fredymosquera.round2008;
+
+public class SavingTheUniverse {
+}
